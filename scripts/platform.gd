d79b69011c4f70a6e9417ad2a25d564faa89768e
kemba <miles.welsh@live.com>
2025-09-13 11:46:04 -0500

    Adding platform.gd for something to drag an object on

---
+++ scripts/platform.gd
@@ -0,0 +1,14 @@
+extends StaticBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	modulate = Color(Color.MEDIUM_SEA_GREEN, 0.6)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Globals.is_dragging:
+		visible = true
+	else:
+		visible = false
